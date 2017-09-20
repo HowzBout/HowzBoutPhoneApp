5e68f751a6c4c0f4b1447fb35901528efd3d27f2
Thomas Annerino <tannerino@howzbout.com>
2017-09-20 04:50:19 -0500

    Added location based code

--- HowzBout/DataViewController.swift
+++ HowzBout/DataViewController.swift
@@ -7,16 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: String = ""
+    
+    @IBOutlet weak var lblText: UILabel!
+    @IBOutlet weak var btnHungry: UIButton!
+    
+    var locationManager: CLLocationManager = CLLocationManager();
+    var longitude:Double = 0.0
+    var latitude:Double = 0.0
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        longitude = locValue.longitude
+        latitude = locValue.latitude
+        print(longitude)
+        print(latitude)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +46,14 @@ class DataViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//    }
 
-    @IBOutlet weak var lblText: UILabel!
-    @IBOutlet weak var btnHungry: UIButton!
+   
 
-    @IBAction func btnHungryClick(_ sender: UIButton) {
-        lblText.text = "I'm Hungry Button Clicked!"
-    }
+//    @IBAction func btnHungryClick(_ sender: UIButton) {
+//        lblText.text = "I'm Hungry Button Clicked!"
+//    }
 }
 
